8dabdb83bbdfa35c73d67075824ca324a596cbf3
pablovicho <pablovvb@gmail.com>
2022-11-12 18:53:57 -0600

    Squashed commit of the following:
    commit 8b10383a8e48da6cd62028780d8b5f06c5481b7b
    Author: pablovicho <pablovvb@gmail.com>
    Date:   Sat Nov 12 18:51:42 2022 -0600
    
        single Article view

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,6 +3,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import '../index.css';
 
 export const Slider = ({mediaArray}) => {
+  console.log(mediaArray)
     const [index, setIndex] = useState(0);
     const [direction, setDirection] = useState(null);
 
@@ -22,7 +23,7 @@ export const Slider = ({mediaArray}) => {
   className='carousel'
   >
 {mediaArray.length > 1 && mediaArray.map((media, i) => (
-      <Carousel.Item key={`slide ${i}`}>
+      <Carousel.Item key={`slide ${i}`} interval={1500}>
       <img
         className= 'd-block carouselImage'
         src={media}
