4548660c4b11f2c4ce6a04955d309302622ee813
pablovicho <pablovvb@gmail.com>
2022-11-22 16:28:35 -0600

    update Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import mediaByIndex from '../utils/sliderMedia'
 
 export default function Home() {
     const description = ["Pinta, sí, pero también canta, escribe, compone, descompone, esculpe y de vez en cuando, escupe palabras radiales.",
-    "Ya no se acuerda dónde nació, pero sabe lo que le creció a orillas del río Pánuco, la imaginación, y lleva por sentado, una resistencia inusual y devota hacia la música, el teatro y el humor,  que se gestó en Xalapa; en el 2022, corre a la península por no salir de México y habitar sus orillas ,le queda de vicio, el abismo.",
+    "Ya no se acuerda dónde nació, pero sabe que creció a orillas del río Pánuco la imaginación. Y lleva por sentado, una resistencia inusual y devota hacia la música, el teatro y el humor, que se gestó en Xalapa en el 2022. Corre a la península por no salir de México y habitar sus orillas ,le queda de vicio, el abismo.",
     "Ha hecho, radio (Como veo doy, Máscara contra cabellera, Tacos de lengua, El triángulo de las no mudas, Deletréalo, y otros), televisión (Tras la noche, Mujeres hechas en casa, El Molcajete, y otros), teatro, cabaret, comedia, stand up, y las lasañas le quedan muy buenas.",
     "Es incapaz de realizar una fiesta infantil como lo dicta la etiqueta, pero los eventos los organiza muy bien, y hasta le pagan por ello. Así, la artista, entre pambazos, pinceles, lienzos en blanco, hijos, cazuelas y novios, se fue forjando a si misma al grado de no poder describir ni quién es, ni qué hace, pase usted, bienvenido al laberinto del artista."]
 
--- src/utils/sliderMedia.js
+++ src/utils/sliderMedia.js
@@ -1,13 +1,22 @@
 const sliderMedia = ["https://res.cloudinary.com/dvesljkuv/image/upload/v1656390536/bullo_galeria_75d6e75c04.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145467/IMG_0461_cf981c8c83.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145467/IMG_0463_a9f34949c4.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145467/IMG_0312_57b8a1526b.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145468/IMG_1201_a2d1e1caf0.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145475/IMG_1750_a1bc9a089d.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145507/DSCN_5109_fb5543684a.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145508/IMG_0654_9f3dcf1310.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145467/IMG_0463_a9f34949c4.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145507/Negative0_15_23_A_1_50781e52ce.jpg",
 "https://res.cloudinary.com/dvesljkuv/image/upload/v1656390566/bullo1_e28df5b51d.jpg",
 "https://res.cloudinary.com/dvesljkuv/image/upload/v1656435744/IMG_2951_6de6a1d739.jpg",
-"https://res.cloudinary.com/dvesljkuv/image/upload/v1656435745/IMG_2970_ad31b3eb2c.jpg",
-"https://res.cloudinary.com/dvesljkuv/image/upload/v1656435744/IMG_6323_c3f1320035.jpg",
 "https://res.cloudinary.com/dvesljkuv/image/upload/v1656435744/IMG_2969_e829aa6da2.jpg",
 "https://res.cloudinary.com/dvesljkuv/image/upload/v1656435743/IMG_3172_ce8e9e4248.jpg",
-"https://res.cloudinary.com/dvesljkuv/image/upload/v1656435743/IMG_5748_5029305e57.jpg",
 "https://res.cloudinary.com/dvesljkuv/image/upload/v1656435743/IMG_3134_1e18ea481a.jpg",
 "https://res.cloudinary.com/dvesljkuv/image/upload/v1656435742/IMG_3159_ad16146b48.jpg",
-"https://res.cloudinary.com/dvesljkuv/image/upload/v1656435742/IMG_3065_9bbf81bccc.jpg"];
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1656435742/IMG_3065_9bbf81bccc.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145508/IMG_0821_6bf53a3364.jpg",
+"https://res.cloudinary.com/dvesljkuv/image/upload/v1669145513/IMG_0613_c0275b1ae0.jpg",
+];
 
 export default sliderMedia;
